1339891ba4eb155eda6eb7b198147d7d18f53bc5
Vasanth Balakrishnan <vasantheb@gmail.com>
2016-05-30 02:09:44 +0530

    Create Card and its specs

---
+++ card.rb
@@ -0,0 +1,25 @@
+class Card
+  attr_reader :type, :value, :rank
+
+  class InvalidCardError < ArgumentError
+  end
+
+  PICTURE_CARDS = {J: 11, Q: 12, K:13, A:14}
+  VALID_TYPES = ['C'.freeze, 'H'.freeze, 'S'.freeze, 'D'.freeze]
+
+  def initialize(string)
+    type = string[0].upcase
+    fail InvalidCardError, 'Invalid card type' unless VALID_TYPES.include? type
+    @type = type
+    @value = string[1..-1].upcase
+    @rank = typecast_value(@value)
+  end
+
+  private
+  def typecast_value(value)
+    return value.to_i unless (value =~ /^[2-9]$|^10$/).nil?
+
+    fail InvalidCardError, "Invalid card value" unless PICTURE_CARDS.has_key? value.to_sym
+    PICTURE_CARDS[value.to_sym]
+  end
+end
---
+++ spec/card_spec.rb
@@ -0,0 +1,69 @@
+require_relative 'spec_helper'
+require_relative '../card'
+
+describe Card do
+  describe 'A new instance' do
+    describe 'Without argument' do
+      it 'raises InvalidCardError' do
+        -> { Card.new }.must_raise ArgumentError
+      end
+    end
+
+    describe 'With an invalid card type' do
+      it 'raises InvalidCardError' do
+        (('A'..'Z').to_a - Card::VALID_TYPES).each do |errs, type|
+          arg_error = -> { Card.new "#{type}10" }.must_raise Card::InvalidCardError
+          arg_error.message.must_equal'Invalid card type'
+        end
+      end
+    end
+
+    describe 'With an invalid card value' do
+      it 'raises InvalidCardError' do
+        %w(C12 DINVALID H01 S00 C1).each do |card|
+          e = -> { Card.new card }.must_raise Card::InvalidCardError
+          e.message.must_equal 'Invalid card value'
+        end
+      end
+    end
+  end
+
+  describe '#type' do
+    let(:club) { Card.new 'C10' }
+    let(:diamond) { Card.new 'DJ' }
+    let(:heart) { Card.new 'HQ' }
+    let(:spade) { Card.new 'S9' }
+
+    it 'gives the type of the card' do
+      club.type.must_equal 'C'
+    end
+  end
+
+  describe '#value' do
+    let(:club) { Card.new 'C2' }
+    let(:diamond) { Card.new 'Dj' }
+    let(:heart) { Card.new 'HQ' }
+    let(:spade) { Card.new 'S10' }
+
+    it 'gives the type of the card' do
+      club.value.must_equal '2'
+      diamond.value.must_equal 'J'
+      heart.value.must_equal 'Q'
+      spade.value.must_equal '10'
+    end
+  end
+
+  describe '#rank' do
+    let(:club) { Card.new 'C2' }
+    let(:diamond) { Card.new 'Dj' }
+    let(:heart) { Card.new 'HQ' }
+    let(:spade) { Card.new 'S10' }
+
+    it 'gives the type of the card' do
+      club.rank.must_equal 2
+      diamond.rank.must_equal 11
+      heart.rank.must_equal 12
+      spade.rank.must_equal 10
+    end
+  end
+end
